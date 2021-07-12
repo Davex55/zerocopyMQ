e57cbbc534e41f78c7f1199972341eb6a3fad9ad
Davex55 <alejandro.benavente.alvarez@alumnos.upm.es>
2021-07-12 11:21:43 +0200

    chapuza

---
+++ broker.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "comun.h"
+#include "diccionario.h"
+#include "cola.h"
+#include <stdint.h>
+
+#include <netdb.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
+
+void error_read(int socketServ, int socketClient);
+void print_colas(char *c, void *v);
+void print_mensajes(void *v);
+void liberar_cola(char *c, void *v);
+void liberar_mensajes(void *v);
+
+int error, error2;
+
+int main(int argc, char *argv[])
+{
+    int socketServ, socketClient, leido;
+    unsigned int tam_dir;
+    struct sockaddr_in dir, dir_cliente;
+    struct iovec iov[3];
+    char *msg, *cola;
+    int *tam = malloc(sizeof(int));
+    char *op = malloc(sizeof(char));
+    int opcion = 1;
+    int ok = 1;
+    int fail = 0;
+
+    fprintf(stdout, "se inicia la ejecucion\n");
+    // comprobacion numero de parametros
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s puerto\n", argv[0]);
+        return 1;
+    }
+
+    // se crea el socket
+    if ((socketServ = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("error creando socket");
+        return 1;
+    }
+
+    // Para reutilizar puerto inmediatamente
+    if (setsockopt(socketServ, SOL_SOCKET, SO_REUSEADDR, &opcion, sizeof(opcion)) < 0) {
+        perror("error en setsockopt");
+        return 1;
+    }
+
+    // Se inicializan los valores del struct dir
+    dir.sin_addr.s_addr = INADDR_ANY;
+    dir.sin_port = htons(atoi(argv[1]));
+    dir.sin_family = PF_INET;
+
+    // 
+    if (bind(socketServ, (struct sockaddr *)&dir, sizeof(dir)) < 0)
+    {
+        perror("error en bind");
+        close(socketServ);
+        return 1;
+    }
+
+    // 
+    if (listen(socketServ, 10) < 0) {
+        perror("error en listen");
+        close(socketServ);
+        return 1;
+    }
+
+    // Inicia estructuras de datos de las colas
+    struct diccionario *d;
+    d = dic_create();
+
+    while (1){
+        tam_dir = sizeof(dir_cliente);
+        if ((socketClient = accept(socketServ, (struct sockaddr *)&dir_cliente, &tam_dir)) < 0) {
+            perror("error en accept");
+            close(socketServ);
+            return 1;
+        }
+
+        fprintf(stdout, "se acepta una nueva peticion\n");
+        if ((leido=read(socketClient, op, sizeof(char))) < 0) {
+            error_read(socketServ, socketClient);
+			return 1;
+		}
+
+        // createMQ
+        if(*op == 'C'){        
+
+            fprintf(stdout, "Se procede a crear una nueva cola\n");                
+            if((leido=read(socketClient, tam, sizeof(int))) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            cola = malloc(*tam);
+            if((leido=recv(socketClient, cola, *tam, MSG_WAITALL)) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            if (dic_put(d, cola, cola_create()) < 0){
+                iov[0].iov_base = &fail; 
+                iov[0].iov_len = sizeof(int);
+            } else {                
+                iov[0].iov_base = &ok; 
+                iov[0].iov_len = sizeof(int);
+            }            
+            writev(socketClient, iov, 1);
+        }
+
+        // destroyMQ
+        else if(*op == 'D'){
+            fprintf(stdout, "Se procede a destruir una cola\n");
+
+            if((leido=read(socketClient, tam, sizeof(int))) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            cola = malloc(*tam);
+            if((leido=recv(socketClient, cola, *tam, MSG_WAITALL)) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+            
+            if(dic_remove_entry(d, cola, liberar_cola) < 0){
+                iov[0].iov_base = &fail; 
+                iov[0].iov_len = sizeof(int);
+            } else {
+                iov[0].iov_base = &ok; 
+                iov[0].iov_len = sizeof(int);                
+            }        
+            writev(socketClient, iov, 1);
+        }
+
+        // put
+        else if(*op == 'P'){
+            fprintf(stdout, "Se procede introducir un nuevo mensaje\n");
+
+            if((leido=read(socketClient, tam, sizeof(int))) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            cola = malloc(*tam);
+            if((leido=recv(socketClient, cola, *tam, MSG_WAITALL)) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            if((leido=read(socketClient, tam, sizeof(int))) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            msg = malloc(*tam);
+            if((leido=recv(socketClient, msg, *tam, MSG_WAITALL)) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            if(cola_push_back(dic_get(d, cola, &error),msg) < 0){
+                iov[0].iov_base = &fail; 
+                iov[0].iov_len = sizeof(int);
+            } else if (error < 0) {
+                iov[0].iov_base = &fail; 
+                iov[0].iov_len = sizeof(int);
+            } else {
+                iov[0].iov_base = &ok; 
+                iov[0].iov_len = sizeof(int); 
+            }
+            writev(socketClient, iov, 1);
+        }
+
+        // get
+        else if(*op == 'G'){
+            fprintf(stdout, "Se procede a extraer un mensaje\n");
+
+            if((leido=read(socketClient, tam, sizeof(int))) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+
+            cola = malloc(*tam);
+            if((leido=recv(socketClient, cola, *tam, MSG_WAITALL)) < 0 ) {
+                error_read(socketServ, socketClient);
+                return 1;
+            }
+            
+            msg = cola_pop_front(dic_get(d, cola, &error), &error2);
+            uint32_t tamMsg;
+            if(error < 0){
+                iov[0].iov_base = &fail; 
+                iov[0].iov_len = sizeof(int);
+                writev(socketClient, iov, 1);
+            } else if(error2 < 0){
+                tamMsg = 0;
+                iov[0].iov_base = &ok; 
+                iov[0].iov_len = sizeof(int);
+                iov[1].iov_base = &tamMsg; 
+                iov[1].iov_len = sizeof(uint32_t);
+                writev(socketClient, iov, 2);      
+            } else {
+                tamMsg = strlen(msg);
+                iov[0].iov_base = &ok; 
+                iov[0].iov_len = sizeof(int);
+                iov[1].iov_base = &tamMsg; 
+                iov[1].iov_len = sizeof(uint32_t); 
+                iov[2].iov_base = msg; 
+                iov[2].iov_len = strlen(msg); 
+                writev(socketClient, iov, 3);
+                free(msg);
+            }
+        }
+
+        printf("Contenido:\n");
+        dic_visit(d, print_colas);
+        close(socketClient);
+    }
+    close(socketServ);
+    return 0;
+}
+
+void print_colas(char *c, void *v){
+    struct cola *cola = v;
+    printf("%s:\n", c);
+    cola_visit(cola, print_mensajes);
+    printf("\n");
+}
+
+void print_mensajes(void *v){
+    char *msg = v;
+    int tam = strlen(msg);
+    printf(" %s, %d", msg, tam);
+}
+
+void liberar_cola(char *c, void *v){
+	struct cola *cola = v;
+	free(c);
+	error = cola_destroy(cola, liberar_mensajes);
+}
+
+void liberar_mensajes(void *v){
+    free(v);
+}
+
+void error_read(int socketServ, int socketClient){
+    perror("error en read");
+    close(socketServ);
+    close(socketClient);
+}
---
+++ libzerocopyMQ.c
@@ -0,0 +1,257 @@
+#include <stdint.h>
+#include "zerocopyMQ.h"
+#include "comun.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
+
+
+#define TAM_COLA 65536
+#define TAM_MSG 4294967296
+
+int createSocket();
+
+int createMQ(const char *cola) {
+    int socketServ, tamCola, leido;
+    struct iovec iov[3];
+    int *result = malloc(sizeof(int));
+    char c = 'C';
+    
+    if((socketServ = createSocket()) == 1 ){
+        return -1;
+    }       
+
+    if((tamCola = strlen(cola) + 1 ) > TAM_COLA){
+        perror("El nombre de la cola es demasiado grande");
+        return -1;
+    }
+
+    iov[0].iov_base = &c; 
+    iov[0].iov_len = sizeof(c);
+    iov[1].iov_base = &tamCola;
+    iov[1].iov_len = sizeof(tamCola);
+    iov[2].iov_base = (char *)cola;
+    iov[2].iov_len = strlen(cola) + 1;
+    
+    writev(socketServ, iov, 3);
+
+    if ((leido = read(socketServ, result, sizeof(int))) < 0) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+
+    close(socketServ);
+
+    if(*result){
+        free(result);
+        return 0;
+    } else {
+        free(result);
+        return -1;
+    }
+}
+
+int destroyMQ(const char *cola){
+    int socketServ, tamCola, leido;
+    struct iovec iov[3];
+    int *result = malloc(sizeof(int));
+    char c = 'D';
+    
+    if((socketServ = createSocket()) == 1 ){
+        return -1;
+    }       
+
+    if((tamCola = strlen(cola) + 1 ) > TAM_COLA){
+        perror("El nombre de la cola es demasiado grande");
+        return -1;
+    }
+
+    iov[0].iov_base = &c; 
+    iov[0].iov_len = sizeof(c);
+    iov[1].iov_base = &tamCola;
+    iov[1].iov_len = sizeof(tamCola);
+    iov[2].iov_base = (char *)cola;
+    iov[2].iov_len = strlen(cola) + 1;
+    
+    writev(socketServ, iov, 3);
+
+    if ((leido = read(socketServ, result, sizeof(int))) < 0) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+
+    close(socketServ);
+
+    if(*result){
+        free(result);    
+        return 0;
+    } else {
+        free(result);
+        return -1;
+    }      
+}
+
+int put(const char *cola, const void *mensaje, uint32_t tam) {
+    int socketServ, tamCola, leido;
+    struct iovec iov[5];
+    int *result = malloc(sizeof(int));
+    char c = 'P';
+    
+    if((socketServ = createSocket()) == 1 ){
+        return -1;
+    }       
+
+    if((tamCola = strlen(cola) + 1 ) > TAM_COLA){
+        perror("El nombre de la cola es demasiado grande");
+        return -1;
+    }
+
+    if(tam == 0){
+        fprintf(stdout, "El mensaje esta vacio\n");
+        return 0;
+    }
+
+    if(tam > TAM_MSG){
+        perror("El mensaje es demasiado grande");
+        return -1;
+    }
+
+	tam = tam + 1;
+    iov[0].iov_base = &c; 
+    iov[0].iov_len = sizeof(c);
+    iov[1].iov_base = &tamCola;
+    iov[1].iov_len = sizeof(tamCola);
+    iov[2].iov_base = (char *)cola;
+    iov[2].iov_len = strlen(cola) + 1;
+    iov[3].iov_base = &tam;
+    iov[3].iov_len = sizeof(tam);
+    iov[4].iov_base = (char *)mensaje;
+    iov[4].iov_len = tam;
+    
+    writev(socketServ, iov, 5);
+
+    if ((leido = read(socketServ, result, sizeof(int))) < 0) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+
+	printf("el tam teorico es: %d\n", tam);
+	printf("el mensaje es: %s\n", (char *) mensaje);
+	int tam2 = strlen((char *) mensaje);
+	printf("el tam real del mensaje es: %d\n", tam2);
+
+    close(socketServ);
+
+    if(*result){
+        free(result);      
+        return 0;
+    } else {
+        free(result);
+        return -1;
+    }      
+}
+
+int get(const char *cola, void **mensaje, uint32_t *tam, bool blocking) {
+    int socketServ, tamCola, leido;
+    struct iovec iov[3];
+    int *result = malloc(sizeof(int));
+    char c = 'G';
+    
+    if((socketServ = createSocket()) == 1 ){
+        return -1;
+    }       
+
+    if((tamCola = strlen(cola) + 1 ) > TAM_COLA){
+        perror("El nombre de la cola es demasiado grande");
+        return -1;
+    }
+
+    iov[0].iov_base = &c; 
+    iov[0].iov_len = sizeof(c);
+    iov[1].iov_base = &tamCola;
+    iov[1].iov_len = sizeof(tamCola);
+    iov[2].iov_base = (char *)cola;
+    iov[2].iov_len = strlen(cola) + 1;
+
+    writev(socketServ, iov, 3);
+
+    if ((leido = read(socketServ, result, sizeof(int))) < 0) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+    
+    if(!*result){
+        free(result);
+        return -1;
+    } 
+
+    if ((leido = read(socketServ, tam, sizeof(uint32_t))) < 0) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+    
+    if(*tam == 0){
+        close(socketServ);
+        free(result);
+        return 0;
+    }
+
+    *mensaje = malloc(*tam);
+    if((leido=recv(socketServ, *mensaje, *tam, MSG_WAITALL)) < 0 ) {
+        perror("error en read");
+        close(socketServ);
+        return -1;
+    }
+
+	printf("el tam teorico es: %d\n", *tam);
+	printf("el mensaje es: %s\n", (char *) *mensaje);
+	int tam2 = strlen((char *) *mensaje);
+	printf("el tam real del mensaje es: %d\n", tam2);
+
+    close(socketServ);
+    free(result);
+    return 0;
+}
+
+int createSocket(){
+    int socketServ;
+	struct sockaddr_in dir;
+	struct hostent *host_info;
+    char *broker_host, *broker_port;
+
+    broker_host = getenv("BROKER_HOST");
+    broker_port = getenv("BROKER_PORT");
+
+	host_info = gethostbyname(broker_host);
+	dir.sin_addr=*(struct in_addr *)host_info->h_addr;
+	dir.sin_port=htons(atoi(broker_port));
+	dir.sin_family=PF_INET;
+
+	if ((socketServ=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("error creando socket");
+		return 1;
+	}
+	
+    if (connect(socketServ, (struct sockaddr *)&dir, sizeof(dir)) < 0) {
+		perror("error en connect");
+		close(socketServ);
+		return 1;
+	}    
+
+    return socketServ;
+}
